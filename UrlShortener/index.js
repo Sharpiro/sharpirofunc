fc6226faa11a4dbd40270d1c8a92d7f47826860b
sharpiro <dsharpbb09@gmail.com>
2017-04-16 22:14:53 -0400

    adding url lengthening code

--- UrlShortener/index.js
+++ UrlShortener/index.js
@@ -1,17 +1,22 @@
+var realurl = require('realurl');
+
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+    var url = "";//req.query.name ? req.body.name : req.body.name;
+    if (req.query.url || (req.body && req.body.url)) {
+        realurl.get(req.query.url, function (error, result) {
+            context.res = {
+                body: { url: result }
+            };
+            context.done();
+        });
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a url on the query string or in the request body"
         };
+        context.done();
     }
-    context.done();
 };
